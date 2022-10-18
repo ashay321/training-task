ea3ef07fb99b5d2ad56c97c240a3d1742b097375
ashay321 <ashay.wanjarkar1@getfareye.com>
2022-10-18 10:59:41 +0530

    updated delete method

--- src/main/java/com/fareye/training/controller/TodoController.java
+++ src/main/java/com/fareye/training/controller/TodoController.java
@@ -3,11 +3,14 @@ import com.fareye.training.model.Todo;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.*;
 import java.util.*;
 
 @RestController
 public class TodoController {
+
+
     private static HashMap<Integer, Todo> todos = new HashMap<Integer, Todo>();
     public static Set<String> titles = new HashSet<>();
     int todoId;
@@ -42,11 +45,13 @@ public class TodoController {
     }
 
     @DeleteMapping("/todos/{id}")
-    public void deleteTask(@PathVariable("id") int id) throws Exception {
+    public void deleteTask(@PathVariable("id") int id, HttpServletResponse response) throws Exception {
 
         if(!todos.containsKey(id)) {
             throw new Exception("ID does not exist");
         }
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+
         titles.remove(todos.get(id).getTitle());
         todos.remove(id);
     }
